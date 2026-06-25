fae482f355fca1a41d86eed8599eb2db1d0037ab
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-06-25 17:52:38 +0530

    Time: 1 ms (100%), Space: 46.4 MB (97.85%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int n=nums.length;
-        int[] arr=new int[n];
-
-        for(int i=0;i<n;i++){
+        int oc=0,ec=0;
+        for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0){
-                arr[i]=0;
-            }else{
-                arr[i]=1;
+                ec++;
             }
+            else oc++;
         }
-
-        Arrays.sort(arr);
-        return arr;
+        for(int i =0;i<ec;i++){
+            nums[i]=0;
+        }
+        for(int i =ec;i<nums.length;i++){
+            nums[i]=1;
+        }
+        return nums;
     }
 }
