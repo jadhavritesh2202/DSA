9a0b4e8d0bac8b8ede9936d82cf2c38f4e85b73b
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-07-07 18:21:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,49 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+class Pair{
+    Node node;
+    int dist;
+    Pair(Node node,int dist){
+        this.node=node;
+        this.dist=dist;
+    }
+}
+class Solution {
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(root,0));
+        
+        while(!q.isEmpty()){
+            Pair p=q.remove();
+            Node node=p.node;
+            int dist=p.dist;
+            map.putIfAbsent(dist,node.data);
+            
+            if(node.left!=null){
+                q.add(new Pair(node.left,dist-1));
+            }
+            if(node.right!=null){
+                q.add(new Pair(node.right,dist+1));
+            }
+           
+        }
+        list.addAll(map.values());
+        return list;
+        
+        
+    }
+}
