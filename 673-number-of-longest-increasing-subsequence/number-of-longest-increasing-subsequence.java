df5cea91c7adb4b8aff13926d3aa5678b1c1e8cc
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-07-24 08:00:45 +0530

    Time: 22 ms (33.05%) | Memory: 45.4 MB (58.79%) - LeetSync

---
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findNumberOfLIS(int[] arr) {
+         int n=arr.length;
+         int[] dp=new int[n];
+         Arrays.fill(dp,1);
+         int[] cnt=new int[n];
+         Arrays.fill(cnt,1);
+        int maxi=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]> arr[j] && dp[j]+1>dp[i]){
+                    dp[i]=dp[j]+1;
+                    cnt[i]=cnt[j];
+                }else if(arr[i]> arr[j] && dp[j]+1==dp[i]){
+                    cnt[i]+=cnt[j];
+                }
+            }
+            maxi=Math.max(maxi,dp[i]);
+
+         }
+         int ans=0;
+         for(int i=0;i<n;i++){
+            if(dp[i]==maxi) ans+=cnt[i];
+         }
+         return ans;
+
+
+
+    }
+}
