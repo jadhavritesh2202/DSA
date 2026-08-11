6ec0173dfd20fde390dcfa6651c77190e32f7417
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-08-11 18:19:23 +0530

    Time: 6 ms (85.55%) | Memory: 46.3 MB (84.18%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,54 @@
+class Pair{
+    int node;
+    int cost;
+    Pair(int node,int cost){
+        this.node=node;
+        this.cost=cost;
+    }
+}
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+         for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+         }
+
+         for(int[] flight:flights){
+            int u=flight[0];
+            int v=flight[1];
+            int cost=flight[2];
+            adj.get(u).add(new Pair(v,cost));
+
+         }
+         int[] dist=new int[n];
+         Arrays.fill(dist,Integer.MAX_VALUE);
+         Queue<Pair> q=new LinkedList<>();
+         int steps=0;
+         q.add(new Pair(src,0));
+
+         dist[src]=0;
+
+         while(!q.isEmpty() && steps<=k){
+            steps++;
+            int size=q.size();
+            while(size-->0){
+                Pair p=q.remove();
+                int node=p.node;
+                int cost=p.cost;
+
+                for(Pair nei:adj.get(node)){
+                    int ele=nei.node;
+                    int price=nei.cost;
+
+                    if(cost+price < dist[ele]){
+                        dist[ele]=cost+price;
+                        q.add(new Pair(ele,cost+price));
+                    }
+                }
+            }
+         }
+
+       return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+
+    }
+}
