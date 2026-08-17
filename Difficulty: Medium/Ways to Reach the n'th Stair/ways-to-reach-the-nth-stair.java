02ca2a0b33a378790830cedf4ab2fef0caea7169
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-08-17 18:20:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -1,16 +1,21 @@
 class Solution {
     public int countWays(int n) {
         // code here
-        if(n<=1) return 1;
-        int a=1;
-        int b=1;
-        int ans=0;
-        for(int i=2;i<=n;i++){
-            ans=a+b;
-            a=b;
-            b=ans;
-        }
-        return ans;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return fun(0,n,dp);
         
+    }
+    private int fun(int idx,int n,int[] dp){
+        if(idx==n){
+            return 1;
+        }
+        if(idx>n){
+            return 0;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        int first=fun(idx+1,n,dp);
+        int second=fun(idx+2,n,dp);
+        return dp[idx]=first+second;
     }
 }
