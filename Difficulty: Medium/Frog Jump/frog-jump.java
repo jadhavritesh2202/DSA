5e7a774a50789549267645f2d69e18cec425ed79
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-08-18 17:44:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,22 +1,24 @@
 class Solution {
     int minCost(int[] height) {
         // code here
+        
         int n=height.length;
         int[] dp=new int[n];
         Arrays.fill(dp,-1);
-        return jump(n-1,height,dp);
+        return solve(n-1,height,dp);
+        
     }
-    int jump(int idx,int[] arr,int[] dp){
-        if(idx==0) return 0;
-        
+    public int solve(int idx,int[] height,int[] dp){
+        //base case
+        if(idx==0){
+            return 0;
+        }
         if(dp[idx]!=-1) return dp[idx];
-        
-        int left=jump(idx-1,arr,dp)+Math.abs(arr[idx]-arr[idx-1]);
+        int left=solve(idx-1,height,dp)+Math.abs(height[idx]-height[idx-1]);
         int right=Integer.MAX_VALUE;
         if(idx>1){
-            right=jump(idx-2,arr,dp)+Math.abs(arr[idx]-arr[idx-2]);
+            right=solve(idx-2,height,dp)+Math.abs(height[idx]-height[idx-2]);
         }
-        
         return dp[idx]=Math.min(left,right);
         
     }
