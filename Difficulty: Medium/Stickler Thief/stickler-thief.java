59b613756e9f9f7ff5dba4b20edd00466517290e
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-08-18 18:15:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -1,20 +1,25 @@
 class Solution {
     public int findMaxSum(int arr[]) {
-        // code here
-        int n=arr.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return fun(n-1,dp,arr);
-    }
-    public int fun(int idx,int[] dp,int[] arr){
-        if(idx==0) return arr[idx];
-        if(idx<0) return 0;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        int Pick= arr[idx] + fun(idx-2,dp,arr);
-        int notPick=0 + fun(idx-1,dp,arr);
-        dp[idx]=Math.max(Pick,notPick);
-        return dp[idx];
+
+        int n = arr.length;
+        int[] dp = new int[n];
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            // Take current element
+            int take = arr[i];
+            if (i > 1) {
+                take += dp[i - 2];
+            }
+
+            // Don't take current element
+            int notTake = dp[i - 1];
+
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[n - 1];
     }
 }
