3c2542c7e0efc4cab2d29d868e57b9c3c54af2b6
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-08-29 11:56:49 +0530

    Time: 63 ms (64.02%) | Memory: 42.7 MB (83.33%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,38 +1,86 @@
+// class Solution {
+//     int max = 0;
+
+//     public int getMaximumGold(int[][] grid) {
+//         int n = grid.length;
+//         int m = grid[0].length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (grid[i][j] != 0) {
+//                     dfs(i, j, 0, grid);
+//                 }
+//             }
+//         }
+//         return max;
+
+//     }
+
+//     public void dfs(int i, int j, int sum, int[][] grid) {
+
+//         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length ||
+//                 grid[i][j] == -1 || grid[i][j] == 0) {
+//             max = Math.max(max, sum);
+//             return;
+//         }
+//         sum += grid[i][j];
+//         int temp = grid[i][j];
+//         grid[i][j] = -1;
+//         int[] dx = { 1, -1, 0, 0 };
+//         int[] dy = { 0, 0, 1, -1 };
+
+//         for (int d = 0; d < 4; d++) {
+//             dfs(i + dx[d], j + dy[d], sum, grid);
+//         }
+
+//         grid[i][j] = temp;
+
+//     }
+// }
 class Solution {
-    int max = 0;
+
+    int maxGold = 0;
 
     public int getMaximumGold(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
                 if (grid[i][j] != 0) {
-                    dfs(i, j, 0, grid);
+                    backtrack(grid, i, j, 0);
                 }
             }
         }
-        return max;
 
+        return maxGold;
     }
 
-    public void dfs(int i, int j, int sum, int[][] grid) {
+    public void backtrack(int[][] grid, int r, int c, int gold) {
 
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length ||
-                grid[i][j] == -1 || grid[i][j] == 0) {
-            max = Math.max(max, sum);
+        // invalid condition
+        if (r < 0 || r >= grid.length ||
+            c < 0 || c >= grid[0].length ||
+            grid[r][c] == 0) {
             return;
         }
-        sum += grid[i][j];
-        int temp = grid[i][j];
-        grid[i][j] = -1;
-        int[] dx = { 1, -1, 0, 0 };
-        int[] dy = { 0, 0, 1, -1 };
 
-        for (int d = 0; d < 4; d++) {
-            dfs(i + dx[d], j + dy[d], sum, grid);
-        }
+        // current gold
+        int value = grid[r][c];
 
-        grid[i][j] = temp;
+        // choose
+        grid[r][c] = 0;
 
+        gold += value;
+
+        // answer
+        maxGold = Math.max(maxGold, gold);
+
+        // explore
+        backtrack(grid, r + 1, c, gold);
+        backtrack(grid, r - 1, c, gold);
+        backtrack(grid, r, c + 1, gold);
+        backtrack(grid, r, c - 1, gold);
+
+        // undo
+        grid[r][c] = value;
     }
 }
