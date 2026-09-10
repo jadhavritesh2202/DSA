c821a817f2340c327f63aee27de0d1fa3b0eceab
Ritesh Jadhav <jadhavritesh2202@gmail.com>
2026-09-10 17:53:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -1,26 +1,28 @@
 class Solution {
-    static int lis(int arr[]) {
+    int[] arr;
+    int[][] dp;
+    public int lis(int arr[]) {
         // code here
+        this.arr=arr;
         int n=arr.length;
-        int[][] dp=new int[n][n+1];
+        dp=new int[n][n+1];
         for(int[] ele:dp){
             Arrays.fill(ele,-1);
         }
-        return helper(0,-1,dp,n,arr);
+        return solve(0,-1);
+        
         
     }
-    public static int helper(int idx,int prevIdx,int[][] dp,int n,int[] arr){
-          if(idx==n) return 0;
-          
-          if(dp[idx][prevIdx+1]!=-1) return dp[idx][prevIdx+1];
-          
-          //not take
-          int len=0+helper(idx+1,prevIdx,dp,n,arr);
-          
-          if(prevIdx==-1 || arr[idx] > arr[prevIdx]){
-             len=Math.max(len,1+helper(idx+1,idx,dp,n,arr));
-          //   dp[idx][prevIdx+1]=len;
-          }
-         return dp[idx][prevIdx+1]=len;
+    public int solve(int idx,int prev){
+        if(idx==arr.length) return 0;
+        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
+        int notTake=0+solve(idx+1,prev);
+        int take=0;
+        
+        if(prev==-1 || arr[idx]>arr[prev]){
+            take=1+solve(idx+1,idx);
+        }
+        
+        return dp[idx][prev+1]=Math.max(notTake,take);
     }
 }
